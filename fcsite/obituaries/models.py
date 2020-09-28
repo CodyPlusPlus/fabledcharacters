b02127c3f688db69599a2bf100096e8e98b2977f
Cody <hellocodystuck@gmail.com>
2020-09-28 16:40:40 -0700

    added model and admin

--- fcsite/obituaries/models.py
+++ fcsite/obituaries/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Obituary(models.Model):
+    name    = models.CharField(max_length=50) # remember that max_length is required for CharField
+    system  = models.CharField(max_length=50, blank=True, null=True)
+    level   = models.PositiveSmallIntegerField()
+    story   = models.CharField(max_length=280)
+
+# remember to makemigrations and migrate whenever you modify this file
